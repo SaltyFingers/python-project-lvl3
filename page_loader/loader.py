ba429411e06576a60eb8ab29e59ff4576423a803
Evchik <elcric66@gmail.com>
2021-11-25 21:57:23 +0300

    step 1 done tmp on step 2

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -1,16 +1,27 @@
 from pathlib import PurePosixPath
 from urllib.parse import urlparse
 import os
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from requests.models import Response
+
 
 DIR_PATH = os.getcwd()
 
 
 def download(url, path=DIR_PATH):
-    path_to_file = os.path.join(DIR_PATH, PurePosixPath(path).relative_to('/'),
-                                get_file_name(url))
-    with open(path_to_file, 'w+'):
-        pass
-    print(path_to_file)
+    # dir_name = os.path.join(DIR_PATH, PurePosixPath(path).relative_to('/'))
+    # url_to_name = change_url(url)
+    path_to_file = os.path.join(DIR_PATH, PurePosixPath(path).relative_to('/'), change_url(url))
+    #  dir_for_files_path = os.path.join(dir_name, get_file_dir_name(url_to_name))
+    raw_response = requests.get(url)
+    raw_response.encoding = 'utf-8'
+
+    with open(path_to_file, 'w+') as file:
+        response = BeautifulSoup(raw_response.text, 'html.parser')
+        file.write(response.prettify())
+    get_image_from_html(path)
     return str(path_to_file)
 
 
@@ -20,7 +31,7 @@ def remove_schema(url):
     return parsed_url.geturl().replace(scheme, '', 1)
 
 
-def get_file_name(url):
+def change_url(url):
     new_name = []
 
     for s in remove_schema(url):
@@ -30,3 +41,20 @@ def get_file_name(url):
             new_name.append(s)
     new_name.append('.html')
     return ''.join(new_name)
+
+
+# def get_file_name(name):
+#     new_name = [name, '.html']
+#     return ''.join(new_name)
+
+
+# def get_file_dir_name(name):
+#     new_name = [name, '._files']
+#     return ''.join(new_name)
+
+
+def get_image_from_html(path):
+    with open(path, 'r') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+        for line in soup.find_all('img'):
+            print(line.get('src'))
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,14 +1,16 @@
 from page_loader.loader import download, get_file_name, remove_schema
+from unittest.mock import Mock
 
 
 def test_download():
-    assert download('https://ru.hexlet.io/courses', '/tmp'
-    ) == """/home/soleny/python-project-lvl3/tmp/ru-hexlet-io-courses.html"""  # noqa
+    mock = Mock()
+    expect = '/home/soleny/python-project-lvl3/tmp/ru-hexlet-io-courses.html'
+    assert download('https://ru.hexlet.io/courses', '/tmp') == expect
 
 
 def test_get_file_name():
-    assert get_file_name('https://ru.hexlet.io/courses'
-    ) == 'ru-hexlet-io-courses.html'  # noqa
+    expect = 'ru-hexlet-io-courses.html'
+    assert get_file_name('https://ru.hexlet.io/courses') == expect
 
 
 def tets_remove_schema():
