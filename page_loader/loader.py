51b07dea55ec3f0b94aa6a408055f5642f7103f8
Evchik <elcric66@gmail.com>
2021-12-01 22:31:59 +0300

    edit function for making img urls, tests

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -4,42 +4,40 @@ from urllib.parse import urljoin, urlparse
 import os
 import requests
 from bs4 import BeautifulSoup
-from requests.models import Response
 
 
 DIR_PATH = os.getcwd()
 
 
 def download(url, path=DIR_PATH):
-    
     if path != DIR_PATH:
-        working_dir = os.path.join(DIR_PATH, 
+        working_dir = os.path.join(DIR_PATH,
                                    PurePosixPath(path).relative_to('/'))
     else:
         working_dir = path
 
     names = {"main_dir": ''.join([make_name_from_url(url), '.html']),
              "files_dir": ''.join([make_name_from_url(url), '_files']),
-    }
+             }
 
     path_to_file = os.path.join(working_dir, names['main_dir'])
     files_dir = os.path.join(working_dir, names['files_dir'])
-    
+
     if not os.path.exists(files_dir):
         os.mkdir(files_dir)
-    
+
     raw_response = requests.get(url)
     raw_response.encoding = 'utf-8'
-
 
     with open(path_to_file, 'w+') as file:
         response = BeautifulSoup(raw_response.text, 'html.parser')
         for line in response.find_all('img'):
-                img_name = make_img_name(line.get('src'))
-                with open(os.path.join(files_dir, img_name), 'ab') as image:
-                    image.write(requests.get(make_correct_img_url(
-                                url, line.get('src'))).content)
-                line['src'] = img_name
+            img_url = line.get('src')
+            img_name = make_img_name(img_url)
+            line['src'] = os.path.join(files_dir, img_name)
+            with open(os.path.join(files_dir, img_name), 'ab') as image:
+                image.write(requests.get(make_correct_img_url(
+                            url, img_url)).content)
         file.write(response.prettify())
     return str(path_to_file)
 
@@ -51,22 +49,20 @@ def remove_schema(url):
 
 
 def make_correct_img_url(url, img_url):
-    pass
-    # parsed_img_url = urlparse(img_url)
-    # parsed_url = urlparse(url)
-    # if not parsed_img_url.netloc:
-    #     if (parsed_url.netloc.endswith('/') or 
-    #         parsed_img_url.path.startswith('/')):
-    #         new_img_url = ''.join([parsed_url.netloc, parsed_img_url.path])
-    #     else:
-    #         new_img_url = '/'.join([parsed_url.netloc, parsed_img_url.path])
-    # if not parsed_img_url.scheme:
-    #     new_img_url = ''.join([parsed_url.scheme, new_img_url])
-    # return new_img_url
+    parsed_img_url = urlparse(img_url)
+    parsed_url = urlparse(url)
+    if not parsed_img_url.netloc and not parsed_img_url.scheme:
+        return urljoin(url, img_url)
+    elif not parsed_img_url.scheme:
+        return parsed_img_url.geturl()._replace(scheme=parsed_url.scheme)
+    else:
+        return img_url
 
 
 def make_name_from_url(url):
     page_name = []
+    if url.endswith('/'):
+        url = url[:-1]
     for s in remove_schema(url):
         if not s.isalnum():
             page_name.append('-')
@@ -77,6 +73,8 @@ def make_name_from_url(url):
 
 def make_img_name(image):
     new_name = []
+    if image.startswith('/'):
+        image = image[1:]
     suffix = pathlib.PurePosixPath(image).suffix
     for s in remove_schema(image)[:-len(suffix)]:
         if not s.isalnum():
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,20 +1,28 @@
-from page_loader.loader import download, get_file_name, remove_schema
+from page_loader.loader import download, make_name_from_url, remove_schema
 from unittest.mock import Mock
+import os
+import pyfakefs
 
 
-def test_download():
-    mock = Mock()
-    expect = '/home/soleny/python-project-lvl3/tmp/ru-hexlet-io-courses.html'
-    assert download('https://ru.hexlet.io/courses', '/tmp') == expect
+# def test_download():
+#     expect = '/home/soleny/python-project-lvl3/tmp/ru-hexlet-io-courses.html'
+#     assert download('https://ru.hexlet.io/courses', '/tmp') == expect
 
 
-# def test_download_images():
-#     download()
+# def test_create_files(fs):
+#     path = os.path.join(os.getcwd(), '/lol')
+#     fs.create_dir('/lol')
+#     path = os.path.join(os.getcwd(), '/lol')
+#     download('https://notepad-plus-plus.org/', '/lol')
+#     assert os.path.exists('/lol/notepad-plus-plus-org.html')
+#     assert os.path.exists('/lol/notepad-plus-plus-org_files/assets-images-notepad4ever.gif')
+#     assert os.path.exists('/lol/notepad-plus-plus-org_files/notepad-plus-plus-org-images-logo.svg')
+  
 
 
-def test_get_file_name():
-    expect = 'ru-hexlet-io-courses.html'
-    assert get_file_name('https://ru.hexlet.io/courses') == expect
+
+def test_make_name_from_url():
+    assert make_name_from_url('https://ru.hexlet.io/courses') == 'ru-hexlet-io-courses'
 
 
 def tets_remove_schema():
