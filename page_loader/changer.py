a8c3bbe93d0a4e2cb4d6772c1f9f299701529dd0
Evchik <elcric66@gmail.com>
2022-01-23 14:19:32 +0300

    edit function for changing links to resources in html file

--- page_loader/changer.py
+++ page_loader/changer.py
@@ -61,5 +61,7 @@ def make_new_line(line, tag, file_path):
         'img': 'src',
         'script': 'src',
         'link': 'href', }
-    line[link_from_tag[tag]] = file_path
+    res_path = os.path.join(os.path.basename(os.path.dirname(file_path)),
+                            os.path.basename(file_path))
+    line[link_from_tag[tag]] = res_path
     return line
