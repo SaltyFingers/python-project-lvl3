2b1e7109c45b52262b55a68606a92826e655142c
Evchik <elcric66@gmail.com>
2022-02-19 23:35:00 +0300

    edit fuctions in namer.py

--- page_loader/namer.py
+++ page_loader/namer.py
@@ -1,20 +1,24 @@
 import pathlib
-from urllib.parse import urlparse, urlsplit, urlunsplit
+from urllib.parse import urlparse, urlsplit, urljoin
 import os
 
 
 def remove_schema(url):
+    # parsed_url = urlparse(url)
+    # scheme = '%s://' % parsed_url.scheme
+    # return parsed_url.geturl().replace(scheme, '', 1)
     parsed_url = urlparse(url)
-    scheme = '%s://' % parsed_url.scheme
-    return parsed_url.geturl().replace(scheme, '', 1)
+    return parsed_url.netloc + parsed_url.path
 
 
 def make_absolute_url(main_url, url):
     split_main_url = urlsplit(main_url)
     split_url = urlsplit(url)
-    absolute_url = split_url._replace(scheme=split_main_url.scheme,
-                                      netloc=split_main_url.netloc)
-    return urlunsplit(absolute_url)
+    base = split_main_url.scheme + '://' + split_main_url.netloc
+    print(base)
+    # absolute_url = split_url._replace(scheme=split_main_url.scheme,
+    #                                   netloc=split_main_url.netloc)
+    return urljoin(base, split_url.path)
 
 
 def remove_excess_symbols(url):
@@ -37,7 +41,6 @@ def replace_symbols_with_dashes(url):
 
 def make_name(url, purpose=None):
     url = remove_schema(remove_excess_symbols(url))
-
     if purpose == 'dir':
         suffix = '_files'
     elif (pathlib.PurePosixPath(url).suffix
